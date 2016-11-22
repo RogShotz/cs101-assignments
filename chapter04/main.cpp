91c3ac20cef4d16d48aa03bc1b217e9962468fd6
rogshotz <rogshotz@gmail.com>
2016-11-22 11:57:31 -0800

    made new repo, formatted better, previous code added

---
+++ chapter04/main.cpp
@@ -0,0 +1,42 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 22, 2016
+ *      Author: rogshotz
+ */
+
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 22, 2016
+ *      Author: rogshotz
+ */
+
+#include<iostream>
+
+int example08() {
+
+	int reward;
+	int grain = 1;
+	int square = 0;
+
+	std::cout << "How much rice would you like to redeem? ";
+	std::cin >> reward;
+
+	while (grain <= reward) {
+		grain = grain * 2;
+		square++;
+	}
+
+	std::cout << "You used "<< square << " square and redeemed " << grain << " rice grains.";
+
+return 0;
+}
+
+int main() {
+
+	example08();
+
+}
+
+
