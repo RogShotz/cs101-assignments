7eac1f4e04360cd05b171aa996e47a2be2fb7469
rogshotz <rogshotz@gmail.com>
2016-11-25 17:58:30 -0800

    Added a whole bunch of examples/formatted chapter05, had no internet for days

--- chapter04/main.cpp
+++ chapter04/main.cpp
@@ -5,9 +5,13 @@
  *      Author: rogshotz
  */
 
+#include<algorithm>
+#include<cmath>
 #include<iostream>
+#include<string>
+#include<vector>
 
-int example08() {
+int example08() { //riceman
 
 	int reward;
 	int grain = 1;
@@ -27,21 +31,103 @@ int example08() {
 	return 0;
 }
 
-int example15() {
+int example11() { //output primes to 100
+	for (int n = 2; n <= 100; n++) {
+		for (int y = 2; y * y <= n; y++) {
+			if (n % y == 0) {
+				break;
+			} else if (y + 1 > sqrt(n)) {
+				std::cout << n << " ";
 
-	int y = 2;
+			}
+
+		}
+	}
+
+	return 0;
+}
+
+int example15() { // input to see if prime
+
 	int n;
+	while (n != 0) {
+		std::cout << "Input a number to see if it is a prime: ";
+		std::cin >> n;
 
-	std::cout << "Input a number to see if it is a prime: ";
-	std::cin >> n;
-	if (n % y == 0) {
-		std::cout << n << " is not a prime";
-	} else {
-		std::cout << n << " is a prime";
+		for (int y = 2; y < n; y++) {
+			if (n % y == 0) {
+				std::cout << n << " is not a prime" << std::endl;
+				break;
+			}
+			if (y == n - 1) {
+				std::cout << n << " is a prime" << std::endl;
+			}
+		}
+	}
+	return 0;
+}
+
+int example19() { // input/ output multiple names
+	std::vector<std::string> names;
+	std::vector<int> scores;
+	std::string sTemp;
+	int iTemp;
+
+	for (int x; x >= 0; x++) {
+		std::cin >> sTemp;
+		std::cin >> iTemp;
+		if (sTemp != "NoName") {
+			names.push_back(sTemp);
+			scores.push_back(iTemp);
+		}
+		if (sTemp == "NoName" && iTemp == 0) {
+			for (int i = 0; i < scores.size(); i++) {
+				std::cout << names[i] << " " << scores[i] << " " << std::endl;
+				// figure out how to sort ^ std::sort(scores.begin(), scores.end());
+			}
+		}
+	}
+
+	return 0;
+}
+
+int example20() { //Rip this entire function
+	std::vector<std::string> names;
+	std::vector<int> scores;
+	std::string sTemp;
+	std::string search;
+	int iTemp;
+
+	std::cout
+			<< "Enter a name a space and a score, when done inputting, add NoName 0"
+			<< std::endl;
+	for (int x; x >= 0; x++) {
+		std::cin >> sTemp;
+		std::cin >> iTemp;
+		if (sTemp != "NoName") {
+			names.push_back(sTemp);
+			scores.push_back(iTemp);
+		}
+		while (sTemp == "NoName" && iTemp == 0) {
+			std::cout << "Enter a name to search a score for: ";
+			std::cin >> search;
+			for (int i = 0; true; i++) { //Rip this for loop :(
+				if (i >= names.size()) {
+					std::cout << "No Name Found" << std::endl;
+					break;
+
+				} else if (search == names[i]) {
+					std::cout << search << " has a score of " << scores[i]
+							<< std::endl;
+					i = 0;
+					break;
+				}
+			}
+		}
 	}
 	return 0;
 }
 
 int main() { //used for testing the functions
-
+	example20();
 }
---
+++ chapter05/example05/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 25, 2016
+ *      Author: rogshotz
+ */
+
+#include<algorithm>
+#include<cmath>
+#include<iostream>
+#include<string>
+#include<vector>
+
+int main() {
+
+	return 0;
+}
+
