bb4bb22f576dea111ecb8de77aef6eb5a9ebb801
rogshotz <rogshotz@gmail.com>
2017-11-16 19:08:22 -0800

    Finished the drill

--- chapter03/drill/src/main.cpp
+++ chapter03/drill/src/main.cpp
@@ -9,18 +9,21 @@ int main(){
 
 	std::cout << "Enter the name of the person you want to write to." << std::endl;
 	std::cin >> fName;
-	std::cout << "Dear " << fName << ","
-	std::cout << "    I am sorry to say this, but there will be no water game this year."
-	std::cout << "Please add the name of another friend and the sex associated with them. ([m/f] seperated from their name with an [enter]" << std::endl;
+	std::cout << "Dear " << fName << "," << std::endl;
+	std::cout << "    I am sorry to say this, but there will be no water game this year." << std::endl;
+	std::cout << "Please add the name of another friend and then the sex associated with them [m/f]. (Please seperate the inputs with a [enter]" << std::endl;
 	std::cin >> ofName;
 	std::cin >> ofSex;
 
 	while(breaker != true){	
 		if(ofSex == "m"){
-			std::cout << "If you see " << ofName << " please ask him to call me."	      }
+			std::cout << "If you see " << ofName << " please ask him to call me." << std::endl;
+			breaker = true;
+	}
 		if(ofSex == "f"){
-                	std::cout << "If you see " << ofName << " please ask her to call me."
-		}
+                	std::cout << "If you see " << ofName << " please ask her to call me." << std::endl;
+			breaker = true;		
+}
 		if(ofSex != "m" && ofSex != "f"){
 			std::cout << "Your previous gender was not accepted, please enter either [m] or [f]." << std::endl;
 			std::cin >> ofSex;
@@ -28,19 +31,22 @@ int main(){
 	}
 
 	std::cout << "Dear " << fName << " ," << std::endl;
- std::cout << "    I am sorry to say this, but there will be no water game this year. "      
-	breaker = true;
+ std::cout << "    I am sorry to say this, but there will be no water game this year. ";
+	
+	breaker = false;
 	while(breaker != true){
                 if(ofSex == "m"){
                         std::cout << "If you see " << ofName << " please ask him to call me." << std::endl;
+			breaker = true;		
 		}
                 if(ofSex == "f"){
                         std::cout << "If you see " << ofName << " please ask her to call me." << std::endl;
+			breaker = true;
 		}
         }
 	
-	std::cout << "Yours sincerely," << std::endl << std::endl;
-	std::cout << "    Luke Rowe";
+	std::cout << "Yours sincerely," << std::endl << std::endl << std::endl;
+	std::cout << "    Luke Rowe" << std::endl;
 	
 return 0;
 }
